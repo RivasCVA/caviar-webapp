46e1c8d0d8ea4acb1d4f835aff99279df3405eec
Carlos Rivas <rivascarlosva@gmail.com>
2021-05-18 22:16:09 -0500

    Add sandbox environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from 'pages/HomePage';
+import SandboxPage from 'pages/SandboxPage';
 
 const App = (): JSX.Element => {
     return (
         <main>
             <Switch>
                 <Route exact path="/" component={HomePage} />
+                <Route path="/sandbox" component={SandboxPage} />
             </Switch>
         </main>
     );
