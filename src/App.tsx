8ade0830510cffeb9e5b54d660cf227138f8c877
Carlos Rivas <rivascarlosva@gmail.com>
2021-05-18 21:54:54 -0500

    Add routing with

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from 'pages/HomePage';
 
 const App = (): JSX.Element => {
     return (
-        <div>
-            <HomePage />
-        </div>
+        <main>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+            </Switch>
+        </main>
     );
 };
 
