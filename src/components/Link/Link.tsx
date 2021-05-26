b2959cbcf995262e3c8aa718fa3e8980991c4ec8
Ellis Alcantara <47289830+EIIis@users.noreply.github.com>
2021-05-26 17:57:51 -0500

    Link component (#18)
    * Link feature added
    
    * Changed sandbox printed code
    
    * Fixed issues

---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import Color from 'util/Color';
+
+type Props = {
+    children: string;
+    href: string;
+    selected?: boolean;
+};
+
+const Link = (props: Props): JSX.Element => {
+    const { children, href, selected } = props;
+    return (
+        <a className={css([styles.container, selected && styles.selected])} href={href} type="link">
+            {children}
+        </a>
+    );
+};
+
+Link.defaultProps = {
+    selected: false,
+};
+
+export default Link;
+
+const styles = StyleSheet.create({
+    container: {
+        color: Color.jungleGreen,
+        width: 175,
+        height: 50,
+        fontSize: 24,
+        cursor: 'pointer',
+    },
+    selected: {
+        color: Color.lightBlue,
+    },
+});
--- src/util/Color.ts
+++ src/util/Color.ts
@@ -3,7 +3,9 @@ const Color = {
     offWhite: '#fffeee',
     black: '#000000',
     purple: '#ba55d3',
-    blue: '#5499C7',
+    blue: '#5499c7',
+    jungleGreen: '#8fbc97',
+    lightBlue: '#bdcdd9',
 };
 
 export default Color;
