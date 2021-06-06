17a56772ed43fbcdaf9e5c2f67ce27410513f872
Carlos <60367213+RivasCVA@users.noreply.github.com>
2021-06-06 00:59:30 -0500

    Add  section (#24)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
+import supply from './supply.svg';
+import burn from './burn.svg';
+import cleanup from './cleanup.svg';
+import redistribution from './redistribution.svg';
+import taxes from './taxes.svg';
 
 const icons = {
     logo,
+    supply,
+    burn,
+    cleanup,
+    redistribution,
+    taxes,
 };
 
 export default icons;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import Color from 'util/Color';
+import Tokenomics, { TokenomicsItem } from './components/Tokenomics';
 
 const HomePage = (): JSX.Element => {
     return (
         <div className={css(styles.container)}>
-            <h1>Hello, World!</h1>
+            {/** Title */}
+            <h1>Caviar</h1>
+            {/** Tokenomics Section */}
+            <Tokenomics title="Tokenomics">
+                <TokenomicsItem icon="supply" title="Supply">
+                    100 trillion Caviar tokens
+                </TokenomicsItem>
+                <TokenomicsItem icon="taxes" title="Taxes">
+                    10% tax on all transactions
+                </TokenomicsItem>
+                <TokenomicsItem icon="burn" title="Burn">
+                    3% burned on each transaction
+                </TokenomicsItem>
+                <TokenomicsItem icon="redistribution" title="Redistribution">
+                    5% redistribution to shareholders
+                </TokenomicsItem>
+                <TokenomicsItem icon="cleanup" title="Cleanup">
+                    2% donated towards ocean cleanup
+                </TokenomicsItem>
+            </Tokenomics>
         </div>
     );
 };
@@ -16,8 +36,8 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         alignItems: 'center',
         minHeight: '100vh',
-        backgroundColor: Color.blue,
-        color: Color.white,
+        backgroundColor: Color.oceanWhite,
+        color: Color.black,
     },
 });
 
