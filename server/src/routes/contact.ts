779795dcc0ea4c0138c50091b34df934e5f27b7f
Carlos Rivas <rivascarlosva@gmail.com>
2021-06-26 14:57:07 -0500

    Setup MongoDB with Mongoose

---
+++ server/src/routes/contact.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { name, email } = req.body;
+    const { error } = contactValidation.validate({ name, email });
+
+    if (error) {
+        res.status(400).send(error.message);
+        return;
+    }
+
+    const contact = new Contact({ name, email });
+
+    try {
+        await contact.save();
+        res.send(contact);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+const contactValidation = Joi.object({
+    name: Joi.string().max(255).required(),
+    email: Joi.string().min(5).max(255).email().required(),
+});
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+export default router;
---
+++ server/src/util/debug.ts
@@ -0,0 +1,5 @@
+import debug from 'debug';
+
+export default class Debug {
+    public static db = debug('server:db');
+}
